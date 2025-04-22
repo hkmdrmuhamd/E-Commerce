8a1a9a9ad870632f3850f80a3c1e192fd60d445a
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-22 16:11:31 +0300

    GetProducts and GetProductList APIs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Commerce.Controllers
 {
@@ -6,22 +8,43 @@ namespace E_Commerce.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetProducts()
+        private readonly DataContext _context;
+        
+        public ProductsController(DataContext context)
         {
+            _context = context;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetProduct(int id)
+        public async Task<IActionResult> GetProduct(int? id)
         {
 
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+           
+            if(product == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(product);
         }
 
-        [HttpPost]
-        public IActionResult CreateProduct([FromBody] Product product)
-        {
+        //[HttpPost]
+        //public IActionResult CreateProduct([FromBody] Product product)
+        //{
 
-        }
+        //}
     }
 }
