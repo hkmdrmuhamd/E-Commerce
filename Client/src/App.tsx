39e20fa2ec7e9c1ca4a9ce1dcb38b4316e8d9408
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-22 16:53:02 +0300

    React JSX

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,3 +1,9 @@
+const  products = [
+  {name: "product 1", price: 1000},
+  {name: "product 2", price: 2000},
+  {name: "product 3", price: 3000},
+];
+
 function App() {
   return (
     <> {/*Kapsayıcı element kullanmamızın sebebi bir Parent COmponent içerisinde birden fazla Child componenet kullanırsak bunları bir kapsayıcı elementin içine almamız gereklidir.*/}
@@ -17,14 +23,18 @@ function ProductList() {
   return (
     <>
       <h2>Selam sana demir pençe</h2>
-      <Product />
+      <Product product={products[0]}/>
+      <Product product={products[1]}/>
+      <Product product={products[2]}/>
     </>
   );
 }
 
-function Product() {
+function Product(props: any) { //props: any dediğimiz yapı props adında bir değişken oluştur ve bunun tipi any olsun yani tipsiz olsun demektir. 
   return (
-    <h3>Product</h3>
+    <>
+      <h3>{ props.product.name }</h3> <p>{ props.product.price }</p>
+    </>
   );
 }
 
