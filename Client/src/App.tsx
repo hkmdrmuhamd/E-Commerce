b187198349a0d654344ef07dabdc7c1902b14a72
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-22 17:40:39 +0300

    React useEffect

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -18,13 +18,19 @@ function Header() {
 function ProductList() {
 
   const [products, setProducts] = useState([
-    {id: 1, name: "product 1", price: 1000, is_Active: true},
-    {id: 2, name: "product 2", price: 2000, is_Active: false},
-    {id: 3, name: "product 3", price: 3000, is_Active: true},
+    {id: 1, name: "product 1", price: 1000, isActive: true},
+    {id: 2, name: "product 2", price: 2000, isActive: false},
+    {id: 3, name: "product 3", price: 3000, isActive: true},
   ]);
 
+  useEffect(() => {
+    fetch("https://localhost:7190/api/products")
+    .then(response => response.json()) //gelen response json formatına dönüştürüldü
+    .then(data => setProducts(data)); //setProducts ile data state'i güncellendi(yani products isimli diziye response'lar aktarıldı)
+  }, []);
+
   function addProduct(){
-    setProducts([...products, { id: 4, name: "product 4", price: 4000, is_Active: true }]); //...products listenin var olan değerleridir. Burada yapılan işlem var olan elemanların üstüne yeni bir değer eklememizi sağlar
+    setProducts([...products, { id: 4, name: "product 4", price: 4000, isActive: true }]); //...products listenin var olan değerleridir. Burada yapılan işlem var olan elemanların üstüne yeni bir değer eklememizi sağlar
   }
 
   return (
@@ -44,7 +50,7 @@ function ProductList() {
 function Product(props: any) { //props: any dediğimiz yapı props adında bir değişken oluştur ve bunun tipi any olsun yani tipsiz olsun demektir. 
   return (
     <>
-      { props.product.is_Active ? ( //is_Active değeri true olanlar getirilsin 
+      { props.product.isActive ? ( //is_Active değeri true olanlar getirilsin 
         <div>
           <h3>{ props.product.name }</h3>
           <p>{ props.product.price }</p>
