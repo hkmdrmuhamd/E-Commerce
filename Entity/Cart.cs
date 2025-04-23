8f4c307d1b40a6429e1f8501c92f4e819a7a0d5a
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-23 22:36:47 +0300

    Update

--- Entity/Cart.cs
+++ Entity/Cart.cs
@@ -45,7 +45,6 @@
         public int ProductId { get; set; }
         public Product Product { get; set; } = null!;
         public int CartId { get; set; }
-        public Cart Cart { get; set; } = null!;
         public int Quantity { get; set; }
     }
 }
