5cde86819917bb0764dbd7415456dfc56ace3214
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-23 16:41:37 +0300

    ProductDetailsPage

--- Client/src/pages/catalog/CatalogPage.tsx
+++ Client/src/pages/catalog/CatalogPage.tsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import ProductList from "./ProductList";
 import { IProduct } from "../../model/IProduct";
+import { CircularProgress } from "@mui/material";
 
 export default function CatalogPage() {
 
     const [products, setProducts] = useState<IProduct[]>([]); //Ürün listesini tutmak için IProduct tipinde bir dizi olarak state tanımlıyoruz
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch("https://localhost:7190/api/products")
         .then(response => response.json()) //gelen response json formatına dönüştürüldü
-        .then(data => setProducts(data)); //setProducts ile data state'i güncellendi(yani products isimli diziye response'lar aktarıldı)
+        .then(data => setProducts(data)) //setProducts ile data state'i güncellendi(yani products isimli diziye response'lar aktarıldı)
+        .finally(() => setLoading(false));
     }, []);
+
+    if(loading) return <CircularProgress />;
 
     return(
         <ProductList products={products} />
--- Client/src/pages/catalog/ProductDetails.tsx
+++ Client/src/pages/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { CircularProgress, Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IProduct } from "../../model/IProduct";
@@ -19,11 +19,38 @@ export default function ProductDetailsPage() {
       .finally(() => setLoading(false));
   }, [id]) //id bilgisi her değiştiğinde ilgili component render edilir
 
-  if(loading) return <h5>Loading...</h5>;
+  if(loading) return <CircularProgress />;
 
   if(!product) return <h5>Product Not Found</h5>;
   
   return (
-    <Typography variant="h2">{product.name}</Typography>
+    <Grid container spacing={6}>
+      <Grid size={{ xl:3, lg: 4, md: 5, sm: 6,  xs:12 }}>
+        <img src={`https://localhost:7190/images/${product.imageUrl}`} style={{width: "100%"}}/>
+      </Grid>
+      <Grid size={{ xl:9, lg: 8, md: 7, sm: 6,  xs:12 }}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb:2 }} />
+        <Typography variant="h4" color="secondary">{(product.price / 100).toFixed(2)}₺</Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Stock</TableCell>
+                <TableCell>{product.stock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 }
