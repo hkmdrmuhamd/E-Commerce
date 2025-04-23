c5c331a40400c453c76372ba99b4a40d39d4825c
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-23 22:15:30 +0300

    Update

---
+++ Migrations/20250423190447_RemoveUnnecessaryProperties.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Commerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnnecessaryProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "CartItem");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "CartItem",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "CartItem",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "CartItem",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
