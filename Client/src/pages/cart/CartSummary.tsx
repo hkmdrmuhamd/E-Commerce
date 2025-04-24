0ac01936d8c074eb32aa236f8582b20a03c3cd77
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-24 14:41:20 +0300

    Cart Summary

---
+++ Client/src/pages/cart/CartSummary.tsx
@@ -0,0 +1,27 @@
+import { TableCell, TableRow } from "@mui/material";
+import { useCartContext } from "../../context/CartContext";
+import { currencyTRY } from "../../utils/formatCurrency";
+
+export default function CartSummary() {
+    const { cart } = useCartContext();
+    const subTotal = cart?.cartItems.reduce((toplam, item) =>toplam + (item.price * item.quantity), 0) ?? 0 //en toplam hesplanırkenki 0 toplam değerinin başlangıç değeridir. Ör: int toplam = 0; gibi. En sonraki 0'da subTotal başlangıçta undefined almak yerine 0 olsun demektir
+    const tax = subTotal * 0.2
+    const totalPrice = subTotal + tax;
+    return(
+        <>
+            <TableRow>
+                <TableCell align="right" colSpan={5}>Ara Toplam</TableCell>
+                <TableCell align="right">{currencyTRY.format(subTotal)}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell align="right" colSpan={5}>Vergi (%20)</TableCell>
+                <TableCell align="right">{currencyTRY.format(tax)}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell align="right" colSpan={5}>Toplam</TableCell>
+                <TableCell align="right">{currencyTRY.format(totalPrice)}</TableCell>
+            </TableRow>
+        </>
+        
+    );
+}
