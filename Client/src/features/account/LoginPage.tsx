e3dc5a1f19dc843191f458caf1a440d14084a571
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-25 22:14:50 +0300

    Form Validations

--- Client/src/features/account/LoginPage.tsx
+++ Client/src/features/account/LoginPage.tsx
@@ -1,10 +1,11 @@
 import { LockOutline } from "@mui/icons-material";
-import { Avatar, Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import { Avatar, Box, Container, Paper, TextField, Typography } from "@mui/material";
 import { FieldValues, useForm } from "react-hook-form"
 import request from "../../api/request";
+import { LoadingButton } from "@mui/lab";
 
 export default function LoginPage() {
-    const { register, handleSubmit } = useForm({
+    const { register, handleSubmit, formState: {errors, isSubmitting, isValid} } = useForm({
         defaultValues: {
             username: "",
             password: ""
@@ -24,21 +25,37 @@ export default function LoginPage() {
                 <Typography component="h1" variant="h5" sx={{ textAlign: "center"}}>Login</Typography>
                 <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{mt: 2}}>
                     <TextField 
-                        {...register("username")}
+                        {...register("username",{ required: "username is required" })}
                         label="Enter username" 
                         fullWidth required autoFocus 
                         sx={{ mb: 2 }} 
                         size="small"
+                        error={!!errors.username} //ilk ! olmadığı durum geçerli olsun anlamına gelmektedir yani klasik if(!errors.username) buranın başına ikinci !'i koyunca bu ifadenin boolean tipinde true ya da false yazılmasını sağlıyoruz yani şöyle olur !!errors.username = true ya da false olacak
+                        helperText={errors.username?.message}
                     ></TextField>
                     <TextField 
-                        {...register("password")}
+                        {...register("password",{ required: "password is required", minLength: {
+                            value: 6,
+                            message: "Min length is 6 characters"
+                        } })}
                         label="Enter password" 
                         type = "password" 
                         fullWidth required 
                         sx={{ mb: 2 }} 
                         size="small"
+                        error={!!errors.password}
+                        helperText={errors.password?.message}
                     ></TextField>
-                    <Button type="submit" variant="contained" fullWidth sx = {{ mt: 1 }}>Login</Button>
+                    <LoadingButton 
+                        loading={isSubmitting}
+                        disabled={!isValid} //Form valid olmadığı durumlarda buton çalışmasın
+                        type="submit" 
+                        variant="contained" 
+                        fullWidth 
+                        sx = {{ mt: 1 }}
+                    >
+                        Login
+                    </LoadingButton>
                 </Box>
             </Paper>
         </Container>
