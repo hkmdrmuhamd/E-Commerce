f62ae306ec13f770ea4288825e8309d03434b10d
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-25 15:35:33 +0300

    Authentication

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using E_Commerce.DTO.UserDtos;
+using E_Commerce.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
+        {
+            var user = await _userManager.FindByNameAsync(userLoginDto.UserName);
+            if (user == null)
+            {
+                return BadRequest(new {message = "Girilen kullanıcı adı hatalı."});
+            }
+
+            var result = await _userManager.CheckPasswordAsync(user, userLoginDto.Password);
+            
+            if (!result)
+            {
+                return Unauthorized();
+            }
+            return Ok(new { token = "token" });
+        }
+    }
+}
