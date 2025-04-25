c64355b2f27ed12424dad96ed2aa59e15ac27e99
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2025-04-25 15:35:49 +0300

    AutoMapper

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using E_Commerce.DTO.UserDtos;
+using E_Commerce.Entity;
+
+namespace E_Commerce.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserRegisterDto, AppUser>().ReverseMap();
+
+            CreateMap<AppUser, GetUserDto>().ReverseMap();
+        }
+    }
+}
